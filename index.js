72068e00f60e2ad4171a2363962613b0edcaa896
Ankit Yadav <ankityadav200124@gmail.com>
2022-07-18 18:36:00 +0530

    Save button and paragraph done

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let count =0
 let countEl = document.getElementById("count-el")
+let saveEL = document.getElementById("save-el")
 function increment(){
     count+=1
     countEl.innerText=count
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEL.innerText += countStr
+    console.log(count)
 }
